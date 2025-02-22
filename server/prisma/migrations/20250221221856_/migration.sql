f2b96f14f054a2befc50b645f951d20351098cf1
Arthur Rodrigues <arthur9713@hotmail.com>
2025-02-21 21:09:22 -0300

    refactor: generate new migration file

--- server/prisma/migrations/20250221221856_/migration.sql
+++ server/prisma/migrations/20250221221856_/migration.sql
@@ -1,7 +1,7 @@
 -- CreateTable
 CREATE TABLE `User` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `email` VARCHAR(50) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
     `password` VARCHAR(191) NOT NULL,
     `role` ENUM('PARENT', 'TEACHER', 'SCHOOL') NOT NULL,
 
@@ -12,28 +12,28 @@ CREATE TABLE `User` (
 -- CreateTable
 CREATE TABLE `School` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `userId` INTEGER NOT NULL,
     `nameStreetId` VARCHAR(191) NOT NULL,
     `name` VARCHAR(191) NOT NULL,
     `fixedPeriod` ENUM('PERIODO', 'MES', 'BIMESTRE', 'SEMESTRE', 'ANO') NOT NULL,
+    `userId` INTEGER NOT NULL,
 
-    UNIQUE INDEX `School_userId_key`(`userId`),
     UNIQUE INDEX `School_nameStreetId_key`(`nameStreetId`),
+    UNIQUE INDEX `School_userId_key`(`userId`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `SchoolAddress` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `schoolId` INTEGER NOT NULL,
     `zipCode` INTEGER NOT NULL,
-    `country` VARCHAR(100) NOT NULL,
-    `state` VARCHAR(100) NOT NULL,
-    `city` VARCHAR(100) NOT NULL,
-    `neighborhood` VARCHAR(100) NOT NULL,
-    `street` VARCHAR(100) NOT NULL,
+    `country` VARCHAR(191) NOT NULL,
+    `state` VARCHAR(191) NOT NULL,
+    `city` VARCHAR(191) NOT NULL,
+    `neighborhood` VARCHAR(191) NOT NULL,
+    `street` VARCHAR(191) NOT NULL,
     `number` INTEGER NOT NULL,
-    `complement` VARCHAR(100) NULL,
+    `complement` VARCHAR(191) NULL,
+    `schoolId` INTEGER NOT NULL,
 
     UNIQUE INDEX `SchoolAddress_schoolId_key`(`schoolId`),
     PRIMARY KEY (`id`)
@@ -42,9 +42,9 @@ CREATE TABLE `SchoolAddress` (
 -- CreateTable
 CREATE TABLE `schools_teachers` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `isApproved` BOOLEAN NOT NULL DEFAULT false,
     `schoolId` INTEGER NOT NULL,
     `teacherId` INTEGER NOT NULL,
-    `isApproved` BOOLEAN NOT NULL DEFAULT false,
 
     UNIQUE INDEX `schools_teachers_schoolId_teacherId_key`(`schoolId`, `teacherId`),
     PRIMARY KEY (`id`)
@@ -53,6 +53,7 @@ CREATE TABLE `schools_teachers` (
 -- CreateTable
 CREATE TABLE `schools_parents` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `isApproved` BOOLEAN NOT NULL DEFAULT false,
     `schoolId` INTEGER NOT NULL,
     `parentId` INTEGER NOT NULL,
 
@@ -65,9 +66,9 @@ CREATE TABLE `schools_parents` (
 -- CreateTable
 CREATE TABLE `Teacher` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `userId` INTEGER NOT NULL,
     `name` VARCHAR(191) NOT NULL,
     `subjects` VARCHAR(191) NOT NULL,
+    `userId` INTEGER NOT NULL,
 
     UNIQUE INDEX `Teacher_userId_key`(`userId`),
     PRIMARY KEY (`id`)
@@ -76,8 +77,8 @@ CREATE TABLE `Teacher` (
 -- CreateTable
 CREATE TABLE `Parent` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `userId` INTEGER NOT NULL,
     `name` VARCHAR(191) NOT NULL,
+    `userId` INTEGER NOT NULL,
 
     UNIQUE INDEX `Parent_userId_key`(`userId`),
     PRIMARY KEY (`id`)
@@ -86,9 +87,9 @@ CREATE TABLE `Parent` (
 -- CreateTable
 CREATE TABLE `Student` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
     `schoolId` INTEGER NOT NULL,
     `parentId` INTEGER NOT NULL,
-    `name` VARCHAR(191) NOT NULL,
 
     UNIQUE INDEX `Student_schoolId_key`(`schoolId`),
     UNIQUE INDEX `Student_parentId_key`(`parentId`),
@@ -99,37 +100,22 @@ CREATE TABLE `Student` (
 CREATE TABLE `Grade` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `typeGrade` ENUM('AVALIACAO', 'TRABALHO', 'EXERCICIO', 'EXTRA') NOT NULL,
-    `date` DATETIME(3) NOT NULL,
     `value` INTEGER NOT NULL,
     `total` INTEGER NOT NULL,
     `numberedPerid` INTEGER NOT NULL,
+    `date` DATETIME(3) NOT NULL,
     `studentId` INTEGER NOT NULL,
 
-    UNIQUE INDEX `Grade_studentId_key`(`studentId`),
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- CreateTable
-CREATE TABLE `TotalGrade` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `value` INTEGER NOT NULL,
-    `numberedPerid` INTEGER NOT NULL,
-    `fixedPeriod` ENUM('PERIODO', 'MES', 'BIMESTRE', 'SEMESTRE', 'ANO') NOT NULL,
-    `year` INTEGER NOT NULL,
-    `studentId` INTEGER NOT NULL,
-
-    UNIQUE INDEX `TotalGrade_studentId_key`(`studentId`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `Observation` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `date` DATETIME(3) NOT NULL,
     `description` VARCHAR(191) NOT NULL,
+    `date` DATETIME(3) NOT NULL,
     `studentId` INTEGER NOT NULL,
 
-    UNIQUE INDEX `Observation_studentId_key`(`studentId`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -165,9 +151,6 @@ ALTER TABLE `Student` ADD CONSTRAINT `Student_parentId_fkey` FOREIGN KEY (`paren
 
 -- AddForeignKey
 ALTER TABLE `Grade` ADD CONSTRAINT `Grade_studentId_fkey` FOREIGN KEY (`studentId`) REFERENCES `Student`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE `TotalGrade` ADD CONSTRAINT `TotalGrade_studentId_fkey` FOREIGN KEY (`studentId`) REFERENCES `Student`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `Observation` ADD CONSTRAINT `Observation_studentId_fkey` FOREIGN KEY (`studentId`) REFERENCES `Student`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
