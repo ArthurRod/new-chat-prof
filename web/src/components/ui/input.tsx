7735f4b51902f63f310de8f7020d7e55059c653a
Arthur Rodrigues <arthur9713@hotmail.com>
2025-02-24 17:48:14 -0300

    fix: update input classes and label deprecated params

--- web/src/components/ui/input.tsx
+++ web/src/components/ui/input.tsx
@@ -8,7 +8,7 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
       <input
         type={type}
         className={cn(
-          "border-input bg-background ring-offset-background placeholder:text-muted-foreground flex h-10 w-full rounded border px-3 py-2 text-base duration-300 ease-in-out file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-secondary focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primaryDark focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          "border-input ring-offset-background placeholder:text-muted-foreground flex h-10 w-full rounded border bg-white px-3 py-2 text-base duration-300 ease-in-out file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-secondary focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primaryDark focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
           className,
         )}
         ref={ref}
