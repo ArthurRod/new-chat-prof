0987f82e46ce131fd6b28d37fb525fad73363947
Arthur Rodrigues <arthur9713@hotmail.com>
2025-02-24 17:44:01 -0300

    feat: add form ui `shadcn` components

---
+++ web/src/components/ui/input.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+
+import { cn } from "@/lib/utils";
+
+const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
+  ({ className, type, ...props }, ref) => {
+    return (
+      <input
+        type={type}
+        className={cn(
+          "border-input bg-background ring-offset-background placeholder:text-muted-foreground flex h-10 w-full rounded border px-3 py-2 text-base duration-300 ease-in-out file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-secondary focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primaryDark focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          className,
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
+  },
+);
+Input.displayName = "Input";
+
+export { Input };
