3cf2a7613547b3f78e1485effdd148d333ebcfae
Arthur Rodrigues <arthur9713@hotmail.com>
2025-02-26 17:44:32 -0300

    feat: update schemas

--- server/services/teacher/schemas/UserSchema.ts
+++ server/services/teacher/schemas/UserSchema.ts
@@ -1,27 +1,32 @@
 import {Role} from "@prisma/client";
 import {z} from "zod";
 
-export const CreateUserFullBodySchema = z.object({
-  email: z.string().min(6).max(50).email({
-    message: "Invalid email format.",
-  }),
-  password: z
-    .string()
-    .min(6)
-    .max(20)
-    .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/, {
-      message: "Invalid password format.",
+export const CreateUserFullBodySchema = z
+  .object({
+    email: z.string().min(6).max(50).email({
+      message: "Invalid email format.",
     }),
-  confirmPassword: z
-    .string()
-    .min(6)
-    .max(20)
-    .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/, {
-      message: "Invalid password format.",
+    password: z
+      .string()
+      .min(6)
+      .max(20)
+      .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/, {
+        message: "Invalid password format.",
+      }),
+    confirmPassword: z
+      .string()
+      .min(6)
+      .max(20)
+      .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/, {
+        message: "Invalid password format.",
+      }),
+    role: z.nativeEnum(Role, {
+      errorMap: () => {
+        return {message: "Select a valid user type."};
+      },
     }),
-  role: z.nativeEnum(Role, {
-    errorMap: () => {
-      return {message: "Select a valid user type."};
-    },
-  }),
-});
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "As senhas não estão iguais",
+    path: ["customConfirmError"],
+  });
--- server/services/user/schemas/UserSchema.ts
+++ server/services/user/schemas/UserSchema.ts
@@ -19,27 +19,32 @@ export const CreateUserSchema = z.object({
   }),
 });
 
-export const CreateUserFullBodySchema = z.object({
-  email: z.string().min(6).max(50).email({
-    message: "Invalid email format.",
-  }),
-  password: z
-    .string()
-    .min(6)
-    .max(20)
-    .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/, {
-      message: "Invalid password format.",
+export const CreateUserFullBodySchema = z
+  .object({
+    email: z.string().min(6).max(50).email({
+      message: "Invalid email format.",
     }),
-  confirmPassword: z
-    .string()
-    .min(6)
-    .max(20)
-    .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/, {
-      message: "Invalid password format.",
+    password: z
+      .string()
+      .min(6)
+      .max(20)
+      .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/, {
+        message: "Invalid password format.",
+      }),
+    confirmPassword: z
+      .string()
+      .min(6)
+      .max(20)
+      .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/, {
+        message: "Invalid password format.",
+      }),
+    role: z.nativeEnum(Role, {
+      errorMap: () => {
+        return {message: "Select a valid user type."};
+      },
     }),
-  role: z.nativeEnum(Role, {
-    errorMap: () => {
-      return {message: "Select a valid user type."};
-    },
-  }),
-});
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "As senhas não estão iguais",
+    path: ["customConfirmError"],
+  });
